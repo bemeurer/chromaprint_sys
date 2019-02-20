ff60e64fa766ebd9873803004434ace3a4f27ffe
Bernardo Meurer <meurerbernardo@gmail.com>
2019-02-19 21:28:57 -0800

    Fixed static build

--- build.rs
+++ build.rs
@@ -1,8 +1,8 @@
 use bindgen;
-#[cfg(feature = "dynamic")]
-use metadeps;
 #[cfg(feature = "static")]
 use cmake;
+#[cfg(feature = "dynamic")]
+use metadeps;
 
 use std::path::PathBuf;
 
@@ -15,43 +15,40 @@ fn create_builder() -> bindgen::Builder {
 fn write_bindings(builder: bindgen::Builder) {
     let bindings = builder.generate().expect("Unable to generate bindings");
     let out_path = PathBuf::from(std::env::var("OUT_DIR").unwrap());
-    bindings.write_to_file(out_path.join("bindings.rs")).expect("Couldn't write bindings!");
-}
-
-#[cfg(feature = "dynamic")]
-fn get_dynamic_chromaprint() -> Vec<PathBuf> {
-    // Make sure we have chromaprint
-    let libs = metadeps::probe().unwrap();
-    libs["libchromaprint"].include_paths.to_owned()
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
 
 #[cfg(feature = "dynamic")]
 fn include_dynamic() {
-        let mut builder = create_builder();
-        let include_paths = get_dynamic_chromaprint();
-        for path in include_paths {
-            builder = builder.clang_arg(format!("-I{:?}", path))
-        }
-        write_bindings(builder);
-}
+    let mut builder = create_builder();
 
-#[cfg(feature = "static")]
-fn build_chromaprint() -> PathBuf {
-    cmake::Config::new("vendor").define("CMAKE_BUILD_TYPE", "RELEASE").build()
-}
+    // Make sure we have chromaprint
+    let libs = metadeps::probe().unwrap();
+    let include_paths = libs["libchromaprint"].include_paths.to_owned();
+    for path in include_paths {
+        builder = builder.clang_arg(format!("-I{:?}", path))
+    }
 
-#[cfg(feature = "static")]
-fn link_chromaprint(lib_path: PathBuf){
-    println!("cargo:rustc-link-search=native={}", lib_path.display());
-    println!("cargo:rustc-link-lib=static=vendor");
+    write_bindings(builder);
 }
 
 #[cfg(feature = "static")]
 fn include_static() {
-        let lib_path = build_chromaprint();
-        link_chromaprint(lib_path);
-        let builder = create_builder().clang_arg("-Ivendor/src");
-        write_bindings(builder);
+    let builder = create_builder().clang_arg("-Ivendor/src");
+
+    let lib_path = cmake::Config::new("chromaprint")
+        .define("CMAKE_BUILD_TYPE", "RELEASE")
+        .define("BUILD_SHARED_LIBS", "OFF")
+        .build();
+    println!(
+        "cargo:rustc-link-search=native={}",
+        lib_path.join("lib").display()
+    );
+    println!("cargo:rustc-link-lib=static=chromaprint");
+
+    write_bindings(builder);
 }
 
 fn main() {
