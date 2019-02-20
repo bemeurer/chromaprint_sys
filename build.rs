5e297cd10919e3bd3017f01638024b5e5809b6b7
Bernardo Meurer <meurerbernardo@gmail.com>
2019-02-19 19:46:41 -0800

    Submodule madness

--- build.rs
+++ build.rs
@@ -4,44 +4,59 @@ use metadeps;
 #[cfg(feature = "static")]
 use cmake;
 
+use std::path::PathBuf;
+
+fn create_builder() -> bindgen::Builder {
+    bindgen::Builder::default()
+        .header("wrapper.h")
+        .no_copy("(?i)mutex")
+}
+
+fn write_bindings(builder: bindgen::Builder) {
+    let bindings = builder.generate().expect("Unable to generate bindings");
+    let out_path = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    bindings.write_to_file(out_path.join("bindings.rs")).expect("Couldn't write bindings!");
+}
+
 #[cfg(feature = "dynamic")]
-fn get_dynamic_chromaprint(mut bindings: bindgen::Builder) -> bindgen::Builder {
+fn get_dynamic_chromaprint() -> Vec<PathBuf> {
     // Make sure we have chromaprint
     let libs = metadeps::probe().unwrap();
-    for path in &libs["libchromaprint"].include_paths {
-        bindings = bindings.clang_arg(format!("-I{:?}", path))
-    }
-    bindings
+    libs["libchromaprint"].include_paths.to_owned()
+}
+
+#[cfg(feature = "dynamic")]
+fn include_dynamic() {
+        let mut builder = create_builder();
+        let include_paths = get_dynamic_chromaprint();
+        for path in include_paths {
+            builder = builder.clang_arg(format!("-I{:?}", path))
+        }
+        write_bindings(builder);
 }
 
 #[cfg(feature = "static")]
-fn build_chromaprint() -> std::path::PathBuf {
+fn build_chromaprint() -> PathBuf {
     cmake::Config::new("vendor").define("CMAKE_BUILD_TYPE", "RELEASE").build()
 }
 
 #[cfg(feature = "static")]
-fn link_chromaprint(lib_path: std::path::PathBuf){
+fn link_chromaprint(lib_path: PathBuf){
     println!("cargo:rustc-link-search=native={}", lib_path.display());
     println!("cargo:rustc-link-lib=static=vendor");
 }
 
-fn main() {
-    // Generate bindings
-    let mut bindings = bindgen::Builder::default()
-        .header("wrapper.h")
-        .no_copy("(?i)mutex");
-
-    if cfg!(feature = "dynamic") {
-        bindings = get_dynamic_chromaprint(bindings);
-    } else if cfg!(feature = "static") {
-        #[cfg(feature = "static")]
+#[cfg(feature = "static")]
+fn include_static() {
         let lib_path = build_chromaprint();
-        #[cfg(feature = "static")]
         link_chromaprint(lib_path);
-        bindings = bindings.clang_arg("-Ivendor/src")
-    }
+        let builder = create_builder().clang_arg("-Ivendor/src");
+        write_bindings(builder);
+}
 
-    let bindings = bindings.generate().expect("Unable to generate bindings");
-    let out_path = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
-    bindings.write_to_file(out_path.join("bindings.rs")).expect("Couldn't write bindings!");
+fn main() {
+    #[cfg(feature = "dynamic")]
+    include_dynamic();
+    #[cfg(feature = "static")]
+    include_static();
 }
