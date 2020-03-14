e24d1b4c77e4b1e3427897d87a784a2b4a78b9eb
Bernardo Meurer <meurerbernardo@gmail.com>
2020-03-14 01:26:48 -0700

    lib: add dummy test

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,26 @@
 #![allow(clippy::all)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    macro_rules! check {
+        ($f:expr) => {
+            // 1 means success to chromaprint for some reason.
+            assert_eq!($f, 1);
+        };
+    }
+
+    #[test]
+    fn test_dummy_acoustid() {
+        unsafe {
+            let ctx = chromaprint_new(ChromaprintAlgorithm_CHROMAPRINT_ALGORITHM_DEFAULT as i32);
+            check!(chromaprint_start(ctx, 44100, 2));
+            let sample: i16 = 0;
+            check!(chromaprint_feed(ctx, &sample as *const i16, 1));
+            check!(chromaprint_finish(ctx));
+        }
+    }
+}
