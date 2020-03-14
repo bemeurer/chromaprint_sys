a9d7eb274104067419bc213adbb8410a4e566d56
Bernardo Meurer <meurerbernardo@gmail.com>
2020-03-14 00:57:59 -0700

    build: don't bind bindings to var

--- build.rs
+++ build.rs
@@ -3,18 +3,20 @@ use pkg_config;
 use std::path::PathBuf;
 
 fn main() {
+    let out_path = PathBuf::from(std::env::var("OUT_DIR").unwrap());
     pkg_config::Config::new()
         .atleast_version("1.1")
         .probe("libchromaprint")
         .expect("Failed to find libchromaprint");
-    let bindings = bindgen::Builder::default()
-        .no_copy("(?i)mutex")
+    bindgen::Builder::default()
+        .generate_comments(true)
         .header_contents("wrapper.h", "#include<chromaprint.h>")
+        .layout_tests(true)
+        .no_copy("(?i)mutex")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .rustfmt_bindings(true)
         .generate()
-        .expect("Failed to generate chromaprint bindings");
-    let out_path = PathBuf::from(std::env::var("OUT_DIR").unwrap());
-    bindings
+        .expect("Failed to generate chromaprint bindings")
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Failed to write bindings");
 }
